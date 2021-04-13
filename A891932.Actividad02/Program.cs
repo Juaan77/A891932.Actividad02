fc1c371f853b1d88d211478ce4d714777cc98f64
Juan Manuel Arroyo <juanma77@live.com.ar>
2021-04-12 22:18:14 -0300

    Agregada la funcionalidad de listar el catalogo.

--- A891932.Actividad02/Program.cs
+++ A891932.Actividad02/Program.cs
@@ -10,23 +10,105 @@ namespace A891932.Actividad02
     {
         static void Main(string[] args)
         {
-            // Se crea un catalogo cuyo identificador de productos va a ser su ID.
-            Dictionary<int, Producto> Catalogo = new Dictionary<int, Producto>();
+            // Variables y constantes a utilizar
             string opcionElegida;
+            int idProducto;
+            string nombreProducto;
+            int stockProducto;
+            const string solicitarID = "Ingrese el ID del producto:";            
+            const string solicitarNombreProducto = "Ingrese el NOMBRE del producto:";            
+            const string solicitarStockProducto = "Ingrese el STOCK INICIAL del producto:";
+            const string solicitarCantidad = "Ingrese la CANTIDAD del producto:";
+            const string menuPrincipal = "P - Pedidos\nE - Entregas\nI - Añadir producto al catalogo\nL - Mostrar el catalogo actual\nS - SALIR\n";
 
-            do
+            // Catalogo de productos
+            Dictionary<int, Producto> Catalogo = new Dictionary<int, Producto>();
+        
+            void ListarCatalogo()
             {
-                
-            } while (opcionElegida != "s");
+                Console.WriteLine("\tCatalogo de Productos actual:\n");
 
-            // Imprime el estado final del catalogo de productos al salir.
-            Console.WriteLine("\tCatalogo de Productos actual:");
+                if (Catalogo.Count == 0)
+                {
+                    Console.WriteLine("El catalogo no tiene productos actualmente.\n");
+                }
 
-            foreach(KeyValuePair<int, Producto> producto in Catalogo)
-            {
-                Console.WriteLine($"- ID: {producto.Key} | '{producto.Value.Nombre}' \tStock: {producto.Value.Stock}");
+                foreach (KeyValuePair<int, Producto> producto in Catalogo)
+                {
+                    Console.WriteLine($"- ID: {producto.Key} | '{producto.Value.Nombre}' \tStock: {producto.Value.Stock}");                    
+                }
+
+                Console.ReadKey();
             }
 
+
+            // Funcion Principal
+            do
+            {
+                Console.WriteLine("\tBienvenido al Sistema de Control de Stock!" + "\n\n" + menuPrincipal);
+                opcionElegida = Console.ReadLine().ToLower();                
+
+                switch (opcionElegida)
+                {
+                    case "p":
+                        
+                        idProducto = Validador.SolicitarNumero(solicitarID);
+
+                        if (!Catalogo.ContainsKey(idProducto))
+                        {
+                            Console.WriteLine("El producto solicitado no existe.");
+                            Console.ReadKey();
+                            break;
+                        }
+                        else
+                        {                            
+                            Catalogo[idProducto].DisminuirStock(Validador.SolicitarNumero(solicitarCantidad));
+                            Console.WriteLine("Operacion Exitosa! Se ha realizado el pedido.\n");
+                            Console.ReadKey();
+                        }
+                        break;
+
+                    case "e":
+
+                        idProducto = Validador.SolicitarNumero(solicitarID);
+
+                        if (!Catalogo.ContainsKey(idProducto))
+                        {
+                            Console.WriteLine("El producto solicitado no existe.");
+                            Console.ReadKey();
+                            break;
+                        }
+                        else
+                        {
+                            stockProducto = Validador.SolicitarNumero(solicitarCantidad);
+                            Catalogo[idProducto].AumentarStock(Validador.SolicitarNumero(solicitarCantidad));
+                            Console.WriteLine("Operacion Exitosa! Se ha realizado la entrega.\n");
+                            Console.ReadKey();
+                        }
+                        break;
+
+                    case "i":
+
+                        idProducto = Validador.SolicitarNumero(solicitarID);
+                        Console.WriteLine(solicitarNombreProducto);
+                        nombreProducto = Console.ReadLine();
+                        stockProducto = Validador.SolicitarNumero(solicitarStockProducto);
+                        Catalogo.Add(idProducto, new Producto(idProducto, nombreProducto, stockProducto));
+                        Console.WriteLine($"Se ha agregado el producto {idProducto} '{nombreProducto}', con {stockProducto} unidades iniciales.\n");
+                        Console.ReadKey();
+                        break;
+
+                    case "l":
+                        ListarCatalogo();
+                        break;
+                }
+
+
+            } while (opcionElegida != "s");
+
+            //  Imprime el estado final del catalogo de productos al salir.
+            Console.Clear();
+            ListarCatalogo();            
             Console.ReadKey();
         }
     }
